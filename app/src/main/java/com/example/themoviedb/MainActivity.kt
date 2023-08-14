527ef9d01cb371a76c21023ddfea098ba2251d77
Boukaddous Mohamed Amine <Mohamed.Amine.Boukaddous@ibm.com>
2023-08-14 20:04:36 +0100

    BASE

---
+++ app/src/main/java/com/example/themoviedb/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.themoviedb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
